779b7b46dc3fec01dab3d106beaab15c1108dda5
sashage <sashage@users.noreply.github.com>
2024-10-06 10:46:32 +0200

    new styling

--- chrome/background.js
+++ chrome/background.js
@@ -1,11 +1,37 @@
-chrome.action.onClicked.addListener(function (tab) {
-  chrome.scripting.executeScript({
-    target: {tabId: tab.id},
-    files: ['combined.js']
-  });
+let active = false;
 
-  chrome.scripting.insertCSS({
-    target: {tabId: tab.id},
-    files: ['combined.css']
-  });
+chrome.action.onClicked.addListener(async (tab) => {
+  active = !active;
+  if (active) {
+    await chrome.scripting.insertCSS({
+      target: {tabId: tab.id},
+      files: ['combined.css']
+    });
+    await chrome.scripting.executeScript({
+      target: {tabId: tab.id},
+      files: ['combined.js']
+    });
+    chrome.action.setTitle({
+      title: "SelectorGadget (Active)",
+      tabId: tab.id
+    });
+  } else {
+    await chrome.scripting.removeCSS({
+      target: {tabId: tab.id},
+      files: ['combined.css']
+    });
+    await chrome.scripting.executeScript({
+      target: {tabId: tab.id},
+      function: () => {
+        if (window.selector_gadget) {
+          window.selector_gadget.unbindAndRemoveInterface();
+          delete window.selector_gadget;
+        }
+      }
+    });
+    chrome.action.setTitle({
+      title: "SelectorGadget",
+      tabId: tab.id
+    });
+  }
 });
